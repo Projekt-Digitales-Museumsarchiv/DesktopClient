2e99bbeeb3c70df1feee54bdc76b8b6d04f59613
hemm <helmut.hemmeter.hh@gmail.com>
2023-06-21 14:23:16 +0200

    Build-Plattformen gesplittet und Jacoco auf linux ergänzt

---
+++ src/test/java/de/sfnbg/archivdesktopclient/DesktopControllerTest.java
@@ -0,0 +1,16 @@
+package de.sfnbg.archivdesktopclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DesktopControllerTest {
+
+    @Test
+    public void dummyTest() {
+        Integer i=1;
+        assertEquals(1,i);
+
+    }
+  
+}
